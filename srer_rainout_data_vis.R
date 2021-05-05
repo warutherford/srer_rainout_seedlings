bf643eeccd185d68b6938a5fa6621cd4e7ce343e
W. Austin Rutherford <arutherford@email.arizona.edu>
2021-05-04 17:08:33 -0700

    clean up some figs, save some new ones

--- srer_rainout_data_vis.R
+++ srer_rainout_data_vis.R
@@ -49,33 +49,46 @@ tot_surv %>%
   mutate(upper = mean_precip_per + mean_se_per,
          lower = mean_precip_per - mean_se_per)
 
-tot_surv %>%
+bar_surv_fig <- tot_surv %>%
   group_by(precip) %>% 
   summarise(mean_precip_per = (100*mean(mean_surv)/10),
             mean_se_per = (100*mean(se_surv)/10)) %>%
   mutate(upper = mean_precip_per + mean_se_per,
-         lower = mean_precip_per - mean_se_per) %>% 
+         lower = mean_precip_per - mean_se_per) %>%
+  mutate(precip = recode_factor(precip,
+                                       "IR" = "Wet",
+                                      "Control" = "Ambient",
+                                       "RO" = "Drought", .ordered = TRUE)) %>% 
   ggplot(mapping = aes(x = precip, y = mean_precip_per, fill = precip)) +
   geom_bar(stat="identity", color = "black", position=position_dodge()) +
   geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.25, position = position_dodge(), size = 1) +
-  scale_fill_manual(values = c("grey30","red1", "blue1")) +
-  scale_x_discrete(labels = c("Ambient", "Drought", "Wet")) +
+  scale_fill_manual(values = c("blue1","grey30","red1")) +
+  scale_x_discrete(labels = c("Wet","Ambient", "Drought")) +
   labs(y = "Mean Survival (%)",
        x = "Precipitation Treatment") +
   labs_pubr() +
-  theme_pubr(legend = "right")
+  theme_pubr(legend = "none")
+
+ggsave(filename = "Figures_Tables/bar_mean_surv.tiff",
+       plot = bar_surv_fig,
+       dpi = 800,
+       width = 22,
+       height = 12,
+       units = "in",
+       compression = "lzw")
+
 
 # plot survival over time with error bars
 
-labels.precip <- factor(tot_surv$precip, labels = c("Ambient", "Drought", "Wet"))
+#labels.precip <- factor(tot_surv$precip, labels = c("Ambient", "Drought", "Wet"))
 
-tot_surv %>% 
-  mutate(precip = recode(precip,
-    "Control" = "Ambient",
-    "RO" = "Drought",
-    "IR" = "Wet")) %>% 
+line_mean_surv <- tot_surv %>% 
+  mutate(precip = recode_factor(precip,
+                                "Control" = "Ambient",
+                                "IR" = "Wet",
+                                "RO" = "Drought", .ordered = TRUE)) %>% 
   ggplot(aes(x = date, y = 10*mean_surv, group = cohort, color = precip)) + 
-  scale_color_manual(values = c("grey30", "red1", "blue1")) +
+  scale_color_manual(values = c("grey30", "blue1", "red1")) +
   scale_x_date(date_labels = "%b-%Y", date_breaks = "3 months") +
   geom_line(aes(linetype = cohort), stat = "identity", size = 1) + 
   scale_linetype_manual(values=c("solid","longdash", "dotted")) +
@@ -88,6 +101,14 @@ tot_surv %>%
   labs_pubr() +
   facet_wrap(~precip, ncol = 3, nrow = 1) +
   theme_pubr(legend = "bottom", x.text.angle = 45)
+
+ggsave(filename = "Figures_Tables/line_mean_surv.tiff",
+       plot = line_mean_surv,
+       dpi = 800,
+       width = 22,
+       height = 12,
+       units = "in",
+       compression = "lzw")
 
 # create data set for precip and excl
 tot_surv_pe <- seedlings_obs %>% 
@@ -108,9 +129,15 @@ precip_cont_df <- seedlings_obs %>%
 
 # clip vs unclip across exclusion treatments
 precip_cont_df %>%
-  ggplot(aes(x = precip_cont, y = survival, color = clip, group = clip)) + 
+  group_by(precip_cont, precip, clip, excl) %>% 
+  summarise(mean_surv = 100*mean(survival/10)) %>% #convert survival to a percentage
+  ggplot(aes(x = precip_cont, y = mean_surv, color = clip, group = clip)) + 
   geom_smooth(method = "glm", formula = y ~ log(x))+
-  facet_wrap(~excl)
+  labs(y = "Mean Survival (%)",
+       x = "PPT (mm)",
+       color = "Grazing") +
+  labs_pubr() +
+  facet_wrap(~excl, nrow = 1)
 
 # only exclusion txs
 precip_cont_df %>% 
@@ -118,7 +145,7 @@ precip_cont_df %>%
   geom_smooth(method = "glm", formula = y ~ log(x))
 
 # year 2 zeros in total exclusion tx really bring down the averages
-test %>% filter(cohort == "2") %>% 
+test %>% dplyr::filter(cohort == "2") %>% 
   ggplot()+
   geom_histogram(aes(x = survival))+
   facet_wrap(~excl)
